c3c84c596c973890c0bf08238cc17f5bf54f5a27
Rahe22 <141894777+Rahe22@users.noreply.github.com>
2026-02-23 10:41:54 -0600

    Delete service worker registration from script.js
    Removed service worker registration code.

--- script.js
+++ script.js
@@ -97,8 +97,3 @@ function cobrar() {
     renderizarTicket();
     irAMenu();
 }
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("service-worker.js")
-    .then(() => console.log("Service Worker registrado"))
-    .catch(error => console.log("Error:", error));
-}
